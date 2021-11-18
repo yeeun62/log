bf77769d61d8b783f5525f365a5f8c03fe1b4956
yeeun62 <byebye62@naver.com>
2021-11-18 17:45:30 +0900

    .

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const app = express();
 const cors = require("cors");
 const { getDatabase, set, ref, onValue, push } = require("firebase/database");
 const admin = require("firebase-admin");
-const serviceAccount = require("/Users/bang-yeeun/Downloads/handle-id-firebase-adminsdk-4o2u4-25c9c98276.json");
+//const serviceAccount = require("/Users/bang-yeeun/Downloads/handle-id-firebase-adminsdk-4o2u4-25c9c98276.json");
 const ip = require("ip");
 const os = require("os");
 const requestIp = require("request-ip");
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://handle-log.firebaseio.com",
-});
+// admin.initializeApp({
+// 	credential: admin.credential.cert(serviceAccount),
+// 	databaseURL: "https://handle-log.firebaseio.com",
+// });
 
 const config = require("./config");
 const db = getDatabase();
