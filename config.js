f31e0d8065ed82a77764ae68d466b2a8cbb3ff97
yeeun62 <byebye62@naver.com>
2021-11-18 17:51:36 +0900

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
 	apiKey: process.env.API_KEY,
 	authDomain: process.env.AUTH_DOMAIN,
 	databaseURL: process.env.DATABASE_URL,
-	projectId: process.env.PROJECT_ID,
+	projectId: "handle-log", //process.env.PROJECT_ID,
 	storageBucket: process.env.STORAGEBUCKET,
 	messagingSenderId: process.env.MESSAGING_SENDER_ID,
 	appId: process.env.APP_ID,
