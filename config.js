0b9ea011ca8ca2d3d1856d087256c0942a15e0eb
yeeun62 <byebye62@naver.com>
2021-11-19 11:14:24 +0900

    config.js 수정

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const firebaseConfig = {
 	apiKey: process.env.API_KEY,
 	authDomain: process.env.AUTH_DOMAIN,
-	databaseURL: process.env.DATABASE_URL,
+	databaseURL: "https://handle-log.firebaseio.com", // process.env.DATABASE_URL
 	projectId: "handle-log", //process.env.PROJECT_ID,
 	storageBucket: process.env.STORAGEBUCKET,
 	messagingSenderId: process.env.MESSAGING_SENDER_ID,
