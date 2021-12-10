c2406b9dc2900659e0ca563949ab8ad3e7ebfbb1
yeeun62 <byebye62@naver.com>
2021-12-10 14:33:12 +0900

    .

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/handle_log/server
 
+sudo apt install awscli
 export API_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names API_KEY --query Parameters[0].Value | sed 's/"//g')
 export AUTH_DOMAIN=$(aws ssm get-parameters --region ap-northeast-2 --names AUTH_DOMAIN --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_URL=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_URL --query Parameters[0].Value | sed 's/"//g')
